f10ce52f12e80e050aadbc0ed6887d9c7d11bbcc
valentinefleith <valentine.fleith@gmail.com>
2024-01-26 15:22:02 +0100

    add mouse event (still segfault)

--- src/events.c
+++ src/events.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 17:46:58 by vafleith          #+#    #+#             */
-/*   Updated: 2024/01/26 15:07:09 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/01/26 15:21:23 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,8 +37,8 @@ int key_events(int keycode, t_fractal *fractal)
 
 int mouse_events(int button, t_fractal *fractal)
 {
-	if (button == 1)
-		printf("mouse pressed\n");
+	if (button == 4)
+		return shiftreal(fractal, '+');
 }
 
 int close_window(t_fractal *fractal)
