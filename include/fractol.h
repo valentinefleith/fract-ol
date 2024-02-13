811e9919a6d0f21c37a7e822b234734e1b7f87db
valentinefleith <valentine.fleith@gmail.com>
2024-02-13 01:26:33 +0100

    arrange header and norm

--- include/fractol.h
+++ include/fractol.h
@@ -6,117 +6,122 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 17:42:11 by vafleith          #+#    #+#             */
-/*   Updated: 2024/02/13 00:53:13 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/02/13 01:25:53 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
-#define FRACTOL_H
+# define FRACTOL_H
 
-#include <mlx.h>
-#include <stdlib.h>
-#include <math.h>
+# include "keycodes.h"
+# include "libft.h"
+# include <math.h>
+# include <mlx.h>
+# include <stdlib.h>
 
-#include "libft.h"
-#include "keycodes.h"
+# define MALLOC_ERROR 1
 
-#define MALLOC_ERROR 1
-
-#define HEIGHT 960
+# define HEIGHT 960
 //#define HEIGHT 720
-#define WIDTH 1080
+# define WIDTH 1080
 //#define WIDTH 810
-#define MAX_ITERATIONS 130
-#define MIN_ITERATIONS 40
+# define MAX_ITERATIONS 130
+# define MIN_ITERATIONS 40
 
-#define MAX 4
+# define MAX 4
 
-#define WHITE 0x00FFFFFF
-#define BLACK 0x000000
+# define WHITE 0x00FFFFFF
+# define BLACK 0x000000
 
-#define LINE_LENGTH 20
-
+# define LINE_LENGTH 20
 
 typedef struct s_img
 {
-	void *img; // Pointeur vers l'image
-	char *addr; // Adresse des donnees de l'image
-	int bits_per_pixel; // Nombre de bits utilises pour representer un px
-	int line_length; // Nombre de caracteres dans une ligne de l'image
-	int endian; // Format de stockage des couleurs (BE ou LE)
-}		t_img;
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+}				t_img;
 
 typedef struct s_complex
 {
-	double real;
-	double imaginary;
-}
-	t_complex;
+	double		real;
+	double		imaginary;
+}				t_complex;
 
 typedef struct s_fractal
 {
-	void *mlx;
-	void *win;
-	t_img img;
-	int set;
-	int max_iterations;
-	double xmin;
-	double xmax;
-	double ymin;
-	double ymax;
-	double zoom;
-	double shiftx;
-	double shifty;
-	int colorset;
-	int palette[10];
-	int colornb;
-	t_complex current_point;
-	int help;
-	int brightness;
-	int lock;
+	void		*mlx;
+	void		*win;
+	t_img		img;
+	int			set;
+	int			max_iterations;
+	double		xmin;
+	double		xmax;
+	double		ymin;
+	double		ymax;
+	double		zoom;
+	double		shiftx;
+	double		shifty;
+	int			colorset;
+	int			palette[10];
+	int			colornb;
+	t_complex	current_point;
+	int			help;
+	int			brightness;
+	int			lock;
 
-}
-	t_fractal;
+}				t_fractal;
 
 typedef struct s_pixel
 {
-	int x;
-	int y;
-}
-	t_pixel;
+	int			x;
+	int			y;
+}				t_pixel;
 
-void calculate_and_put_pixels(t_fractal *fractal);
-void init_fractal(t_fractal *fractal);
-int exit_program(t_fractal *fractal);
-int key_events(int keycode, t_fractal *fractal);
-int mouse_events(int button, int x, int y, t_fractal *fractal);
-void my_mlx_pixel_put(t_img *img, int x, int y, int color);
-int calculate_pixel_color(t_pixel px, t_fractal *fractal);
-int	create_trgb(int t, int r, int g, int b);
-int change_colors(t_fractal *fractal);
-void fractal_refresh(t_fractal *fractal);
-int shiftreal(t_fractal *fractal, char direction);
-int shiftimaginary(t_fractal *fractal, char direction);
-int zoom_out(t_fractal *fractal);
-t_complex convert_pixel_into_complex(t_pixel px, t_fractal *fractal);
-t_complex rescale_pixel(t_pixel px, t_fractal *fractal);
-int calculate_mandelbrot(t_pixel px, t_fractal *fractal);
-int calculate_burning_ship(t_pixel px, t_fractal *fractal);
-int calculate_julia(t_pixel px, t_fractal *fractal);
-int parse_args(int argc, char **argv, t_fractal *fractal);
-int display_commands(t_fractal *fractal);
-int zoom_in(t_fractal *fractal, int x, int y);
-int change_julias_shape(int x, int y, t_fractal *fractal);
-void display_standard(t_fractal *fractal);
-void init_struct(t_fractal *fractal, t_img img);
-int change_precision(t_fractal *fractal, int keycode);
-int handle_shift(t_fractal *fractal, int keycode);
-int check_limits(int value, int min, int max); 
-int adjust_brightness(int color, int brightness);
-int get_color(int iterations, t_fractal *fractal);
-int change_brightness(t_fractal *fractal, int keycode);
-void init_palette(t_fractal *fractal);
-int mouse_move(int x, int y, t_fractal *fractal);
-void init_hook(t_fractal *fractal);
+// ====== VIEW ======
+int				zoom_out(t_fractal *fractal);
+int				zoom_in(t_fractal *fractal, int x, int y);
+int				shiftreal(t_fractal *fractal, char direction);
+int				shiftimaginary(t_fractal *fractal, char direction);
+
+// ====== FRACTAL SETS =======
+int				calculate_mandelbrot(t_pixel px, t_fractal *fractal);
+int				calculate_burning_ship(t_pixel px, t_fractal *fractal);
+int				calculate_julia(t_pixel px, t_fractal *fractal);
+int				change_julias_shape(int x, int y, t_fractal *fractal);
+
+// ====== DRAWING ======
+int				get_color(int iterations, t_fractal *fractal);
+void			calculate_and_put_pixels(t_fractal *fractal);
+void			my_mlx_pixel_put(t_img *img, int x, int y, int color);
+
+// ====== EVENTS =======
+int				mouse_events(int button, int x, int y, t_fractal *fractal);
+int				key_events(int keycode, t_fractal *fractal);
+int				mouse_move(int x, int y, t_fractal *fractal);
+int				change_colors(t_fractal *fractal);
+int				change_precision(t_fractal *fractal, int keycode);
+int				handle_shift(t_fractal *fractal, int keycode);
+int				change_brightness(t_fractal *fractal, int keycode);
+int				exit_program(t_fractal *fractal);
+
+// ====== DISPLAY ======
+int				display_commands(t_fractal *fractal);
+void			display_standard(t_fractal *fractal);
+
+// ====== INIT =======
+void			init_fractal(t_fractal *fractal);
+void			init_struct(t_fractal *fractal, t_img img);
+void			init_palette(t_fractal *fractal);
+void			init_hook(t_fractal *fractal);
+
+// ====== UTILS ======
+void			fractal_refresh(t_fractal *fractal);
+t_complex		convert_pixel_into_complex(t_pixel px, t_fractal *fractal);
+t_complex		rescale_pixel(t_pixel px, t_fractal *fractal);
+int				parse_args(int argc, char **argv, t_fractal *fractal);
+int				check_limits(int value, int min, int max);
 
 #endif
