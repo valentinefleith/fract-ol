18fa84dc6c0b4e4cfe2362d7616209e21511567a
valentinefleith <valentine.fleith@gmail.com>
2024-02-17 12:33:58 +0100

    add new atod but not fully working yet

--- src/double_conversion.c
+++ src/double_conversion.c
@@ -6,46 +6,89 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 17:13:20 by vafleith          #+#    #+#             */
-/*   Updated: 2024/02/15 19:37:14 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/02/17 12:17:50 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 #include <math.h>
 
+static double ft_get_dot_pos(char *str)
+{
+	double pos;
+	
+	pos = 0;
+	while (*str)
+	{
+		if (*str == '.')
+			return pos;
+		pos++;
+		str++;
+	}
+	return -1;
+}
+
 double ft_atod(char *str)
 {
-	// en partant du principe que l'input est correct
-	int is_neg = 0;
+	int is_neg;
+	double output;
+	double dot_pos;
+	
+	dot_pos = ft_strlen(str) - ft_get_dot_pos(str) - 1;
+
+	if (dot_pos < 0)
+		return ft_atoi(str);
+	is_neg = 0;
 	if (*str == '-')
 	{
 		is_neg = 1;
 		str++;
 	}
-	double output;
-
 	output = 0.0;
-	while (*str && ft_isdigit(*str) && *str != '.')
+	while (*str && (ft_isdigit(*str) || *str == '.'))
 	{
-		output = output * 10 + (*str - '0');
+		if (*str != '.')
+			output = output * 10 + (*str - '0');
 		str++;
-	}
-	if (!*str)
-		return output;
-	str++;
-	int x = 1;
-	while (*str && ft_isdigit(*str))
-	{
-		output = output + (*str - '0') / pow(10, x);
-		str++;
-		x++;
 	}
 	if (is_neg)
-		return -output;
-	return output;
+		return -output / pow(10, dot_pos);
+	return output / pow(10, dot_pos);
 }
 
-static char *ft_strinsert(char *str, char c, int index)
+//double ft_atod(char *str)
+//{
+//	// en partant du principe que l'input est correct
+//	int is_neg = 0;
+//	if (*str == '-')
+//	{
+//		is_neg = 1;
+//		str++;
+//	}
+//	double output;
+//
+//	output = 0.0;
+//	while (*str && ft_isdigit(*str) && *str != '.')
+//	{
+//		output = output * 10 + (*str - '0');
+//		str++;
+//	}
+//	if (!*str)
+//		return output;
+//	str++;
+//	int x = 1;
+//	while (*str && ft_isdigit(*str))
+//	{
+//		output = output + (*str - '0') / pow(10, x);
+//		str++;
+//		x++;
+//	}
+//	if (is_neg)
+//		return -output;
+//	return output;
+//}
+
+static char *ft_strinsert_andfree(char *str, char c, int index)
 {
 	char *new_str = malloc(ft_strlen(str) + 2);
 	if (new_str == NULL)
@@ -65,10 +108,11 @@ static char *ft_strinsert(char *str, char c, int index)
 		dest_index++;
 	}
 	new_str[dest_index] = '\0';
+	free(str);
 	return new_str;
 }
 
-static char *add_starting_zeros(char *str, int nb)
+static char *add_starting_zeros_andfree(char *str, int nb)
 {
 	char *new_str = malloc(1 + ft_strlen(str) + nb);
 	if (new_str == NULL)
@@ -87,7 +131,7 @@ static char *add_starting_zeros(char *str, int nb)
 char *ft_dtoa(double n)
 {
 	int precision = 10000;
-	int dot_index;
+	//int dot_index;
 	char *conversion;
 	char *output;
 	int is_neg;
@@ -102,13 +146,12 @@ char *ft_dtoa(double n)
 	size_t conv_len = ft_strlen(conversion);
 	if (conv_len < 5)
 	{
-		conversion = add_starting_zeros(conversion, 5 - conv_len);
+		conversion = add_starting_zeros_andfree(conversion, 5 - conv_len);
 		conv_len = 5;
 	}
-	dot_index = ft_strlen(conversion) - 4;
-	output = ft_strinsert(conversion, '.', dot_index);
+	//dot_index = ft_strlen(conversion) - 4;
+	output = ft_strinsert_andfree(conversion, '.', ft_strlen(conversion) - 4);
 	if (is_neg)
-		output = ft_strinsert(output, '-', 0); // /!\ LEAKS
-	free(conversion);
+		output = ft_strinsert_andfree(output, '-', 0);
 	return output;
 }
