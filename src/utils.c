b69c76f8747d991e6f9f474be5aa5f0713ced1d7
valentinefleith <valentinefleith@users.noreply.github.com>
2024-02-26 19:58:19 +0100

    remove minus sign but still inversed

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: vafleith <vafleith@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 11:31:18 by vafleith          #+#    #+#             */
-/*   Updated: 2024/02/21 15:33:37 by vafleith         ###   ########.fr       */
+/*   Updated: 2024/02/26 19:57:21 by vafleith         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,12 @@ void	display_string_centered(t_fractal *fractal, int *y, char *str)
 t_complex	rescale_pixel(t_pixel px, t_fractal *fractal)
 {
 	t_complex	point;
-
 	point.real = (px.x * (fractal->xmax - fractal->xmin) / WIDTH)
 		+ fractal->xmin;
 	point.real = point.real * fractal->zoom + fractal->shiftx;
 	point.imaginary = (px.y * (fractal->ymax - fractal->ymin) / HEIGHT)
 		+ fractal->ymin;
-	if (fractal->set == 2 || fractal->set == 1) // maybe only 2 ?
-		point.imaginary = point.imaginary * fractal->zoom + fractal->shifty;
-	else
-		point.imaginary = -point.imaginary * fractal->zoom + fractal->shifty;
+	point.imaginary = point.imaginary * fractal->zoom + fractal->shifty;
 	return (point);
 }
 
